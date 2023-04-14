a799a5cbcf37fdbf052462cb751ed5e6b9694b05
livioBrito <110505341+livioBrito@users.noreply.github.com>
2023-04-14 15:43:33 -0300

    Main

--- SistemaDePostoDeSaude/app.js
+++ SistemaDePostoDeSaude/app.js
@@ -1,20 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const medicoRoutes = require('./src/routes/medicoRoutes');
-const medicamentoRoutes = require('./src/routes/medicamentoRoutes');
-const postoSaudeRoutes = require('./src/routes/postoSaudeRoutes');
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-app.use('/medicos', medicoRoutes);
-app.use('/medicamentos', medicamentoRoutes);
-app.use('/postos', postoSaudeRoutes);
-
+const app = require('./src/routes/routes');
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- SistemaDePostoDeSaude/src/models/db.js
+++ SistemaDePostoDeSaude/src/models/db.js
@@ -1,7 +1,27 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
-db.serialize(() => {
+const insertAdmin = () => {
+  return new Promise((resolve, reject) => {
+    db.get(`SELECT * FROM admin WHERE username = 'admin'`, (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (!row) {
+        db.run(`INSERT INTO admin (username, password) VALUES ('admin', 'admin123');`, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+db.serialize(async () => {
   db.run(`CREATE TABLE postos_saude (
     id INTEGER PRIMARY KEY,
     nome TEXT NOT NULL,
@@ -23,6 +43,17 @@ db.serialize(() => {
     posto_id INTEGER,
     FOREIGN KEY(posto_id) REFERENCES postos_saude(id)
   );`);
+
+  db.run(`CREATE TABLE admin (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+  );`);
+  try {
+    await insertAdmin();
+  } catch (err) {
+    console.error('Erro ao inserir o administrador:', err);
+  }
 });
 
 module.exports = db;
